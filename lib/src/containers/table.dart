6b8a4a8208792589f2fc824bbf1ed0870b584f70
Joe Freeman <joe@freemansoft.com>
2023-07-07 08:42:24 -0400

    make code less embarrassing

--- lib/src/containers/table.dart
+++ lib/src/containers/table.dart
@@ -29,82 +29,31 @@ class _AdaptiveTableState extends State<AdaptiveTable>
     with AdaptiveElementMixin {
   late List<Map<String, dynamic>> columns;
   late List<Map<String, dynamic>> rows;
-  late List<TableRow> tableRows;
-  late MainAxisAlignment horizontalAlignment;
 
   @override
   void initState() {
     super.initState();
     columns = List<Map<String, dynamic>>.from(adaptiveMap["columns"] ?? []);
 
-    // Shold all be Table Rows
+    // Should all be Table Rows
     rows = List<Map<String, dynamic>>.from(adaptiveMap["rows"] ?? []);
 
     // print(format("Table: columns: {} rows: {}", columns.length, rows.length));
-    // TableCell(Widet([Widget])) A TableCell contains a Widget that contains an arbitrary number of widgets
-    tableRows = List<TableRow>.generate(rows.length, (rowNum) {
-      // this row out of all the rows
-      Map<String, dynamic> row = rows[rowNum];
-      //print(format("Row: num:{} - {})", rowNum, row.toString()));
-
-      // All the table cell markup in this row [cell, cell, cell]
-      List<Map<String, dynamic>> rowTableCells =
-          List<Map<String, dynamic>>.from(row["cells"]);
-      //print(format("rowTableCells: row:{} length:{} - {} ", rowNum,
-      //    rowTableCells.length, rowTableCells.toString()));
-
-      // The row markup contains a [TableCells[items]]
-      List<List<dynamic>> rowCellItems =
-          List<List<dynamic>>.generate(rowTableCells.length, (rowNum) {
-        return rowTableCells[rowNum]["items"];
-      });
-      // print(format("rowCellItems: row:{} length:{} - {}", rowNum,
-      //    rowCellItems.length, rowCellItems.toString()));
-
-      List<TableCell> tableCells =
-          List<TableCell>.generate(rowCellItems.length, (col) {
-        List<Map<String, dynamic>> oneCellItems =
-            List<Map<String, dynamic>>.from(rowCellItems[col]);
-        // print(format("oneCellItems: row:{} col:{} widgets in cell:{} - {}",
-        //     rowNum, col, oneCellItems.length, oneCellItems.toString()));
-        return TableCell(
-            child: Scrollbar(
-                child: Wrap(
-                    children: List<Widget>.generate(oneCellItems.length,
-                        (widgetIndex) {
-          //print(oneCellItems[widgetIndex]);
-          return widgetState.cardRegistry.getElement(oneCellItems[widgetIndex]);
-        }))));
-      });
-
-      // print(format("cell children: {}", tableCellChildren));
-      // return TableRow(children: [tableCellChildren]);
-      return TableRow(children: tableCells);
-    });
-
-    //horizontalAlignment = loadHorizontalAlignment();
   }
 
   @override
   Widget build(BuildContext context) {
-    var backgroundColor =
-        getBackgroundColorIfNoBackgroundImageAndNoDefaultStyle(
-      resolver: resolver,
-      adaptiveMap: adaptiveMap,
-      approximateDarkThemeColors: widgetState.widget.approximateDarkThemeColors,
-      brightness: Theme.of(context).brightness,
-    );
-
     return Table(
       border: TableBorder.all(),
       defaultVerticalAlignment: TableCellVerticalAlignment.middle,
       defaultColumnWidth: FlexColumnWidth(),
+      // column width should be picked up from the columns["width"]
       // columnWidths: const <int, TableColumnWidth>{
       //   1: FlexColumnWidth(),
       //   2: FlexColumnWidth(),
       //   3: FlexColumnWidth(),
       // },
-      children: tableRows,
+      children: generateTableRows(rows),
     );
   }
 
@@ -122,5 +71,56 @@ class _AdaptiveTableState extends State<AdaptiveTable>
       default:
         return MainAxisAlignment.start;
     }
+  }
+
+  ///
+  /// Generates all the Table rows for the table [TableRow[TableCell[Widget]]]
+  ///
+  List<TableRow> generateTableRows(List<Map<String, dynamic>> rows) {
+    return List<TableRow>.generate(rows.length, (rowNum) {
+      return generateTableRowWidgets(rows[rowNum]);
+    });
+  }
+
+  ///
+  /// Generates a TableRow for the Table TableRow[TableCell[Widget]]
+  ///
+  TableRow generateTableRowWidgets(Map<String, dynamic> row) {
+    //print(format("Row: num:{} - {})", rowNum, row.toString()));
+
+    // All the table cell markup in this row [cell, cell, cell]
+    List<Map<String, dynamic>> rowTableCells =
+        List<Map<String, dynamic>>.from(row["cells"]);
+    //print(format("rowTableCells: row:{} length:{} - {} ", rowNum,
+    //    rowTableCells.length, rowTableCells.toString()));
+
+    // The row markup contains a [TableCells[items]]
+    List<List<dynamic>> rowCellItems =
+        List<List<dynamic>>.generate(rowTableCells.length, (rowNum) {
+      return rowTableCells[rowNum]["items"];
+    });
+    // print(format("rowCellItems: row:{} length:{} - {}", rowNum,
+    //    rowCellItems.length, rowCellItems.toString()));
+
+    List<TableCell> tableCells =
+        List<TableCell>.generate(rowCellItems.length, (col) {
+      List<Map<String, dynamic>> oneCellItems =
+          List<Map<String, dynamic>>.from(rowCellItems[col]);
+      // TableCell(Widget([Widget])) A TableCell contains a Widget that contains an arbitrary number of widgets
+      // print(format("oneCellItems: row:{} col:{} widgets in cell:{} - {}",
+      //     rowNum, col, oneCellItems.length, oneCellItems.toString()));
+      return TableCell(
+          child: Scrollbar(
+              child: Wrap(
+                  children:
+                      List<Widget>.generate(oneCellItems.length, (widgetIndex) {
+        //print(oneCellItems[widgetIndex]);
+        return widgetState.cardRegistry.getElement(oneCellItems[widgetIndex]);
+      }))));
+    });
+
+    // print(format("cell children: {}", tableCellChildren));
+    // return TableRow(children: [tableCellChildren]);
+    return TableRow(children: tableCells);
   }
 }
